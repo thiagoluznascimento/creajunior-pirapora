8925928fcae6be2d8fff07d701e3f37f2cd18b88
Thiago Luz Nascimento <thiago.nascimento@justicafacil.com.br>
2024-07-12 08:30:44 -0300

    Adiciona Rotas e Bootstrap
    - Instalação das libs bootstrap e router-dom
    - Cria pasta pages
    - Adiciona componente Menu.jsx
    - Adiciona pagina Home.jsx
    - Adiciona pagina NotFound.jsx
    - Modifica arquivo Ap.jsx
    - Modifica arquivo index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import Cabecalho from "./components/Cabecalho";
-import Banner from "./components/Banner";
-import Galeria from "./components/Galeria";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+// import Cabecalho from "./components/Cabecalho";
+// import Banner from "./components/Banner";
+// import Galeria from "./components/Galeria";
 import Footer from "./components/Footer";
+import Menu from "./components/Menu";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+
 
 
 function App() {
   return (
     <>
-      <Cabecalho />
-      <main>
-        <Banner />
-        <Galeria />
-      </main>
+      <BrowserRouter>
+        <Menu />
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="sobrenos" element={<SobreNos />} /> */}
+          {/* <Route path="processoseletivo" element={<ProcessoSeletivo />} /> */}
+          {/* <Route path="eventos" element={<Eventos />} /> */}
+          {/* <Route path="contato" element={<Contato />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
       <Footer nomeAutor = "Thiago Luz" />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,28 @@
+import { Container, Navbar, Nav} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import imagem from "../assets/logo-branca.png";
+
+function Menu() {
+    return(
+        <header>
+            <Navbar className="custom-navbar" variant="dark" expand="md">
+                <Container fluid>
+                    <Link to="/"> <img src={imagem} alt="imagem logo crea-jr" width="200" /></Link>
+                    <Navbar.Toggle></Navbar.Toggle>
+                    <Navbar.Collapse>
+                        <Nav className="ms-auto">
+                            <Link className="nav-link" to="/">HOME</Link>
+                            <Link className="nav-link" to="/sobrenos">SOBRE-NÓS</Link>
+                            <Link className="nav-link" to="processoseletivo">PROCESSO SELETIVO</Link>
+                            <Link className="nav-link" to="/eventos">EVENTOS</Link>
+                            <Link className="nav-link" to="/contato">CONTATO</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+}
+
+export default Menu;
